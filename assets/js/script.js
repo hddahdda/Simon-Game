f95d3436f50c90f886a03f8a2c0eb9f51275b489
hddahdda <hedda9@hotmail.com>
2021-05-15 13:58:09 +0000

    linked audio files to divs in js

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+// Audio files to keys.
+
+$("#key1").click(function () {
+   var key1Audio =  new Audio("assets/sounds/key1.m4a")
+   key1Audio.play();
+});
+
+$("#key2").click(function () {
+   var key2Audio =  new Audio("assets/sounds/key2.m4a")
+   key2Audio.play();
+});
+
+$("#key3").click(function () {
+   var key3Audio =  new Audio("assets/sounds/key3.m4a")
+   key3Audio.play();
+});
+
+$("#key4").click(function () {
+   var key4Audio =  new Audio("assets/sounds/key4.m4a")
+   key4Audio.play();
+});
+
