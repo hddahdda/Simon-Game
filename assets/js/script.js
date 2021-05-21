0c4cfd55a4bd85d1dd41a950b4dad6f799ef8b0d
hddahdda <hedda9@hotmail.com>
2021-05-21 10:13:23 +0000

    added start button click function, linked to h3 and gameOn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
+// variables
+
 const keys = ["key1", "key2", "key3", "key4"]
 let cpuPattern = [];
 let playerPattern = [];
 let level = 0;
 let gameOn = false;
+
+// if start button is clicked, start the game
+
+$("#button").click(function() {
+    if(!gameOn) {
+        $("h3").text("Game Started!")
+        gameOn = true;
+    }
+    
+});
