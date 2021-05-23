8cf948f3174cfe31ece77dee1a598773c70b8d69
hddahdda <hedda9@hotmail.com>
2021-05-23 11:08:33 +0000

    code tidy up

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@
 /* globals $:false */
 
 // variables
-
 const keys = ["key1", "key2", "key3", "key4"];
 let gameOn = false;
 let cpuPattern = [];
@@ -10,89 +9,89 @@ let playerPattern = [];
 let level = 0;
 
 // if start button is clicked, call the function bottomPushed
-$("#button").click(function() { 
-   buttonPushed();
+$("#button").click(function() {
+    buttonPushed();
 });
 
-function buttonPushed(){ 
- if(!gameOn) { 
+function buttonPushed() {
+    if (!gameOn) {
         $("h2").text(""); // removes rules from game as the game is played
         $(".btndiv").addClass("invisible"); // removes the start button 
- 
-      gameOn = true; // Game is On
-      newSequence(); // Calling functions 
-      checkGame();
+
+        gameOn = true; // Game is On
+        newSequence(); // Calling functions 
+        checkGame();
     }
 }
 
 // Get id from clicked buttons here
-$(".btn").on("click", function(event){
- var playerKey = event.target.id; // collecting the id from the key pressed and storing it
-playerPattern.push(playerKey); // pushing the value from the playerKey in to the playePattern variable
+$(".btn").on("click", function(event) {
+    var playerKey = event.target.id; // collecting the id from the key pressed and storing it
+    playerPattern.push(playerKey); // pushing the value from the playerKey in to the playePattern variable
 
-sound(playerKey); // calling the sound function when the key is clicked
-keyDown(playerKey); // changing button styling when the key is clicked
+    sound(playerKey); // calling the sound function when the key is clicked
+    keyDown(playerKey); // changing button styling when the key is clicked
 
-checkGame(playerPattern.length- 1); 
+    checkGame(playerPattern.length - 1);
 });
 
 // function for the game to create a random sequence 
 function newSequence() {
- playerPattern = [];
+    playerPattern = [];
 
-level++;
-  var randomNumber = Math.floor(Math.random() * 4); // The game part
-  var randomKey = keys[randomNumber];
-  cpuPattern.push(randomKey);
+    level++;
+    var randomNumber = Math.floor(Math.random() * 4); // The game part
+    var randomKey = keys[randomNumber];
+    cpuPattern.push(randomKey);
 
 
- keyDown(randomKey);
- sound(randomKey);
+    keyDown(randomKey);
+    sound(randomKey);
 }
 
 // sound and key styling functions
 
 function sound(keyAudio) {
-  var audio = new Audio("assets/sounds/" + keyAudio + ".m4a");
-  setTimeout(function(){
-      audio.play();
-  },100);
+    var audio = new Audio("assets/sounds/" + keyAudio + ".m4a");
+    setTimeout(function() {
+        audio.play();
+    }, 100);
 
-  // temporarily shortened files using timeout. 
+    // temporarily shortened files using timeout. 
 }
 
 // when the key is triggered (either by computer or human), this function creates inline styling using jQuery.
 function keyDown(keyStyle) {
     $("#" + keyStyle).css('background-color', "rgba(232, 230, 211, 0.73)"); //keeping this inline styling as it it a styling that goes upo another class
 
-// By using setTimeout the styling only applies for 1000ms, 
-    setTimeout(function(){
+    // By using setTimeout the styling only applies for 1000ms, 
+    setTimeout(function() {
         $("#" + keyStyle).css('background-color', '');
     }, 500); // for how long the styling will be applied
 }
 
 // Checking game sequence against user sequence 
-function checkGame(checker){
-    if(playerPattern[checker] === cpuPattern[checker]){
+function checkGame(checker) {
+    if (playerPattern[checker] === cpuPattern[checker]) {
         $("h2").text("Level " + level);
         $("h1").addClass("good");
-    if (cpuPattern.length === playerPattern.length) {
-        setTimeout(function(){
-            newSequence();
-        },1000);
-     }
+        if (cpuPattern.length === playerPattern.length) {
+            setTimeout(function() {
+                newSequence();
+            }, 1000);
+        }
     } else {
         $("h2").text("Game Over, Final level: " + level);
         $("#button").text("Restart");
         $(".btndiv").removeClass("invisible");
         newGame();
-        
+
     }
 
 }
 // Refreshes the Game
-function newGame(){
-    $("button").click(function(){
+function newGame() {
+    $("button").click(function() {
         location.reload();
     });
 }
